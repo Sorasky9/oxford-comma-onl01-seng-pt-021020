40c6225c0baf3765f6061979cacd1be8043ce59f
Malcom Fetus <sora.vista@gmail.com>
2020-02-19 18:59:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,8 +5,8 @@ if array.length == 1
   array.join()
 elsif array.length == 2
 array.join( " and ")
-elsif array.length == 3
-  array.join( ",") 
+else array.length == 3
+  array
 #  array.join( "and")
 
 
