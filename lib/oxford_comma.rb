585e00e00b7891d74f914599dd0ef88e0ee654f0
Malcom Fetus <sora.vista@gmail.com>
2020-02-19 18:24:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,16 @@
+require 'pry'
 def oxford_comma(array)
-if array
-  array.join
-elsif array size == 2
-  array.join( " and ")
-elsif size == 3
-  array.join( "and")
+#if array
+#  array.join
+#elsif array size == 2
+#  array.join( " and ")
+#elsif size == 3
+#  array.join( "and")
 
 
 
 
 
 
-end
+#end
 end
