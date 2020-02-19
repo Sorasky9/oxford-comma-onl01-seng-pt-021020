7aeed4bc81689c02a8d989403870834d65cbe9e1
Malcom Fetus <sora.vista@gmail.com>
2020-02-19 18:47:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
 if array.length == 1
   array.join()
 #  array.join
-#elsif array size == 2
+#elsif array== 2
 #  array.join( " and ")
 #elsif size == 3
 #  array.join( "and")
